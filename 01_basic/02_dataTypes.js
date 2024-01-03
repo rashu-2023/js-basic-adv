3d8a78136ee6828686f33ea39042f873f6ef2d56
Rashu Tyagi <135463938+rashu-2023@users.noreply.github.com>
2024-01-03 17:18:48 +0000

    added datatypes notes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version of JS
+
+//alert("Hello") // we are using node js and not the browser.
+
+let nameOfPerson = "Himesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+//bigint
+// string
+// boolean
+// null => standalone value(representation of an empty value)
+// undefined => it is a type in js
+
+//object
+
+console.log(typeof nameOfPerson);
+
